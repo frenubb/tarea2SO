dacb081fbd375919a992d79042f964039891ba7d
Francisco Barraza <francisco.barraza2001@alumnos.ubiobio.cl>
2023-12-02 02:25:41 -0300

    secuencial completado y funcional, falta README

--- secuencial/secuencial.cpp
+++ secuencial/secuencial.cpp
@@ -4,7 +4,7 @@
 using namespace cv;
 using namespace std;
 
-int maint(int argc, char** argv){
+int main(int argc, char** argv){
 	if(argc != 3){
 		cout << "Uso: " << argv[0] << " <imagen_entrada> <imagen_salida>" << endl;
 		return -1;
@@ -13,7 +13,7 @@ int maint(int argc, char** argv){
 	Mat image = imread(argv[1], IMREAD_COLOR);
 	if(image.empty()){
 		cout << "La imagen no se puede leer" << endl;
-		return -1
+		return -1;
 	}
 
 	// metodo conversion
@@ -22,12 +22,12 @@ int maint(int argc, char** argv){
 			Vec3b pixel = image.at<Vec3b>(f, c);
 			int gris_val = 0.299*pixel[2] + 0.587*pixel[1] + 0.114*pixel[0];
 
-			pixel[0] = pixel[1] = pixel[2] = gris_val
+			pixel[0] = pixel[1] = pixel[2] = gris_val;
 
 			image.at<Vec3b>(f, c) = pixel;
 		}
 	}
 
-	imwrite(arg[2], image);
+	imwrite(argv[2], image);
 	return 0;
 }
